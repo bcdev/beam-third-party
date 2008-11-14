138888aa0596cd6267f9e05d4b6e1b460ff4e56b
marcoz <marcoz@d90d2d48-2b33-0410-a973-e72f85796a02>
2008-11-14 13:17:09 +0000

    move junit tests into a separate directory
    git-svn-id: https://www.brockmann-consult.de/svn/os/beam-4.x-contrib-3p/trunk/mssl-stereomatcher@3673 d90d2d48-2b33-0410-a973-e72f85796a02

---
+++ src/test/java/uk/ac/ucl/mssl/climatephysics/imaging/KernelTest.java
@@ -0,0 +1,26 @@
+package uk.ac.ucl.mssl.climatephysics.imaging;
+
+import javax.media.jai.KernelJAI;
+
+import junit.framework.TestCase;
+import uk.ac.ucl.mssl.climatephysics.imaging.GaussianKernelFixedSize;
+
+public class KernelTest extends TestCase {
+	
+	public void printGaussianKernel(KernelJAI gaussianKernel) {
+		float[] data = gaussianKernel.getKernelData();
+		for (int y = 0; y < gaussianKernel.getHeight(); y++) {
+			for (int x = 0; x < gaussianKernel.getWidth(); x++) {
+				int pos = y * gaussianKernel.getWidth()  + x;
+				System.out.format("%d %d %f ", x, y, data[pos]);
+			}
+			System.out.format("\n");
+		}
+	}
+	
+	public void testSimpleKernel() {
+		KernelJAI gaussianKernel = new GaussianKernelFixedSize(21, 5.25f);
+		printGaussianKernel(gaussianKernel);
+	}
+	
+}
